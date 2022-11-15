d67088bc8b9b0ca184e73aa363d97d6c8a3d8a9e
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2022-11-15 22:15:00 +0200

    'update'

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputRef = document.querySelector('#search-box');
+
+inputRef.addEventListener('input', onInputSearch);
+
+function onInputSearch(e) {
+   console.log(inputRef.value);
+
+}
