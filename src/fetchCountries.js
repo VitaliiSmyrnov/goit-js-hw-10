0f8df7ea19f4e6c8a3c049d10e69792f96ed5194
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2022-11-16 19:18:16 +0200

    'update'

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,7 +3,7 @@ const BASE_URL = 'https://restcountries.com/v3.1/name/';
 export function fetchCountries(name) {
    return fetch(`${BASE_URL}${name}?fields=name,capital,population,flags,languages`).then(res => {
       if (res.ok) {
-         return res.json();
+        return res.json();
       } else {
          throw new Error(res.status);
       }
