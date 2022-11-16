2d55eca1dc0f6e29805fef3a34d64e9fdac78b9e
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2022-11-16 12:47:03 +0200

    'update'

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 import {fetchCountries} from './fetchCountries.js';
+import countriesList from './countrieslist.hbs';
 
 const DEBOUNCE_DELAY = 300;
 
 const inputRef = document.querySelector('#search-box');
+const countryContainerRef = document.querySelector('.country-list');
 
 inputRef.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
 
@@ -18,6 +20,10 @@ function onSuccessSearch(value){
    if (value.status === 404) Notify.failure("Oops, there is no country with that name");
    if (value.length > 10) Notify.info("Too many matches found. Please enter a more specific name.");
    if (value.length >= 2 && value.length <= 10){
+      const markup = countriesList(value);
+      console.log(markup);
+      // countryContainerRef.innerHTML = markup;
+      // console.log(value);
 
    }
    
