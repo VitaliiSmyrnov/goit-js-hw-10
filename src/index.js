5152b73515d523e1b6d0d9d9d8a4a35a1fc8098b
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2022-11-16 14:04:16 +0200

    'update'

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 import {fetchCountries} from './fetchCountries.js';
-import countriesList from './countrieslist.hbs';
+import countryList from './templates/countryList.hbs';
+import countryInfo from './templates/countryInfo.hbs';
 
 const DEBOUNCE_DELAY = 300;
 
 const inputRef = document.querySelector('#search-box');
-const countryContainerRef = document.querySelector('.country-list');
+const countryListContainerRef = document.querySelector('.country-list');
+const countryInfoContainerRef = document.querySelector('.country-info');
 
-inputRef.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
 
-function onInputSearch() {
-     const inputVal = inputRef.value.trim();
+function onInputSearch(e) {
+   const inputVal = e.target.value.trim();
      fetchCountries(inputVal).then(onSuccessSearch).catch(onInputError);
 }
 function onSuccessSearch(value){
@@ -20,18 +21,24 @@ function onSuccessSearch(value){
    if (value.status === 404) Notify.failure("Oops, there is no country with that name");
    if (value.length > 10) Notify.info("Too many matches found. Please enter a more specific name.");
    if (value.length >= 2 && value.length <= 10){
-      const markup = countriesList(value);
-      console.log(markup);
-      // countryContainerRef.innerHTML = markup;
+      const markup = countryList(value);
+      // console.log(markup);
       // console.log(value);
-
+      // const markup = value.map(value => {
+      //    return `<li>${value.population}</li>`
+      // })
+      countryListContainerRef.innerHTML = markup;
    }
-   
+   const markup = countryInfo(value);
+   countryInfoContainerRef.innerHTML = markup;
+
       console.log(value);
      
       
-}
+   }
+   
+   function onInputError(error) {
+      console.log(error);
+   }
 
-function onInputError(error) {
-   console.log(error);
-}
+   inputRef.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
